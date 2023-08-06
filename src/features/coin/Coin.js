9138e90fac05f19de412f5e6d9da7672667ce9fe
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-06 20:50:16 +0500

    State in Sibling Component and Other Components

---
+++ src/features/coin/Coin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Coin = () => {
+    return (
+        <div>
+            <span className='value'>Coin: {count}</span>
+        </div>
+    )
+}
+
+export default Coin;
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
 
 function Counter() {
     const [count, setCount] = useState(0);
     return (
         <div>
-            <button className='button' aria-label='Increment value' onClick={() => { setCount(count + 1) }}> + </button>
-            <span className='value'>{count}</span>
             <button className='button' aria-label='Decrement value' onClick={() => { setCount(count - 1) }}> - </button>
+            <button className='button' aria-label='Increment value' onClick={() => { setCount(count + 1) }}> + </button>
         </div>
     )
 }
 
-export default Counter
+export default Counter;
