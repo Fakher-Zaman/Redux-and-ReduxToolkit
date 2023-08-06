cead4f896d4213365e0945ffef0a68e751970704
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-06 21:42:07 +0500

    Create Redux State Slice

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Store } from './app/Store';
 import { Provider } from 'react-redux'
+import { Store } from './app/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
