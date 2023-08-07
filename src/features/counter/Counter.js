b5035fb716336f17483208f120b2261b74b8c24b
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-07 08:04:38 +0500

    useDispatch - Modifying State

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './counterSlice';
 
 function Counter() {
     const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch();
     return (
         <div>
-            <button className='button' aria-label='Decrement value' onClick={() => { }}> - </button>
-            <span className='value'>Coin: {count}</span>
-            <button className='button' aria-label='Increment value' onClick={() => { }}> + </button>
+            <button className='button' aria-label='Decrement value' onClick={() => { dispatch(decrement()) }}> - </button>
+            <span className='value'>Count: {count}</span>
+            <button className='button' aria-label='Increment value' onClick={() => { dispatch(increment()) }}> + </button>
         </div>
     )
 }
