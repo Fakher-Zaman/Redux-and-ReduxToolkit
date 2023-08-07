88f11fe2a67b76c57830f69afbcccd7f67fa6f01
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-07 08:23:10 +0500

    Action Payload

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import { increment, decrement, incrementByAmount } from './counterSlice';
 
 function Counter() {
     const count = useSelector((state) => state.counter.count);
@@ -10,6 +10,8 @@ function Counter() {
             <button className='button' aria-label='Decrement value' onClick={() => { dispatch(decrement()) }}> - </button>
             <span className='value'>Count: {count}</span>
             <button className='button' aria-label='Increment value' onClick={() => { dispatch(increment()) }}> + </button>
+
+            <button className='button' onClick={() => { dispatch(incrementByAmount(10)) }}>Increment by 10</button>
         </div>
     )
 }
