e4c205b8432e5275126d262df7a6c8253c4021cd
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-07 08:12:16 +0500

    State in other Component

--- src/features/coin/Coin.js
+++ src/features/coin/Coin.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Coin = () => {
+    const count = useSelector((state) => state.counter.count);
     return (
-        <div></div>
+        <span className='value'>Count: {count}</span>
     )
 }
 
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { increment, decrement } from './counterSlice';
+import Coin from '../coin/Coin';
 
 function Counter() {
-    const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch();
     return (
         <div>
             <button className='button' aria-label='Decrement value' onClick={() => { dispatch(decrement()) }}> - </button>
-            <span className='value'>Count: {count}</span>
+            <span className='value'><Coin /></span>
             <button className='button' aria-label='Increment value' onClick={() => { dispatch(increment()) }}> + </button>
         </div>
     )
